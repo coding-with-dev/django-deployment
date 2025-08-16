8a19f2ed47ca42b0feaae5d161db8ff8af7e4143
Devendra Sysmarche <devendra.sysmarche@gmail.com>
2025-08-16 21:23:49 +0530

    Blog Image preview in admin list

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Blog
 
 # Register your models here.
-admin.site.register(Blog)
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "image_preview")
+
+    def image_preview(self, obj):
+        if obj.image_url:
+            return format_html('<img src="{}" width="100" height="auto" style="border-radius:5px"/>', obj.image_url.url)
+        return "(No Image)"
+
+    image_preview.short_description = "Preview"
